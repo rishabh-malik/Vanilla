6d8dcaa5426bb904d61adaa68be2019fbf3f2fd1
rishabh-malik <rishabhmalik249@gmail.com>
2017-07-08 12:35:37 +0530

    meet the chatbot

--- app.js
+++ app.js
@@ -8,12 +8,29 @@ const rl=Readline.createInterface({
     terminal:false
 });
 
+const matcher= require('./matcher')
+
 //creating prompt
 rl.setPrompt('> ');
 rl.prompt();
 
 //creating event listener
 rl.on('line',reply=>{
-    console.log(`You said ${reply}`);
-    rl.prompt();
-});
+    matcher(reply,data=>{
+        switch(data.intent){
+            case 'Hello':
+                console.log("A big hello from vanilla")
+                rl.prompt();
+                break;
+            case 'Exit':
+                console.log("Have a great day ahead")
+                process.exit(0);
+                break;    
+            default:{
+                console.log("I didnot understand what you said")
+                rl.prompt();
+
+            }    
+        }
+    });
+   });
